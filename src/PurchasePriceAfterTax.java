e48dc222c71cfecc602de53c205d97ab7cc65570
Andrew-Stewart1 <stewa2a8@mail.uc.edu>
2022-09-18 16:51:03 -0400

    completed purchase pricing after tax

---
+++ src/PurchasePriceAfterTax.java
@@ -0,0 +1,12 @@
+public class PurchasePriceAfterTax {
+
+    public static void main(String[] args){
+    double priceOfPurchase = 75;
+    double priceOfTax;
+    double priceAfterTax;
+
+    priceOfTax = priceOfPurchase * .05;
+    priceAfterTax = priceOfTax + priceOfPurchase;
+        System.out.println("The total price is " + priceOfPurchase + "$ the cost of tax is " + priceOfTax + "$ the total price is " + priceAfterTax + "$");
+    }
+}
