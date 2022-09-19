3d0cceca58e40250b135d73ab4eccf00773d4146
Andrew-Stewart1 <stewa2a8@mail.uc.edu>
2022-09-18 21:21:44 -0400

    completed purchase pricing after tax

---
+++ YearlyMaintenanceCost.java
@@ -0,0 +1,16 @@
+public class YearlyMaintenanceCost {
+
+    public static void main(String[] args){
+
+        double springCost = 100;
+        double summerCost = 75.55;
+        double fallCost = 105.43;
+        double winterCost = 99.99;
+        double totalYearlyMaintenanceCost;
+
+        totalYearlyMaintenanceCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("The total yearly maintenance cost is: " + totalYearlyMaintenanceCost);
+    }
+}
+
